a6d1788a69baec53477664f58f38d2828b9ad8ca
Isaiah Adams <iadams1@atu.edu>
2026-02-02 20:11:08 -0600

    Plaid check

---
+++ lib/mywallet/mywallet.dart
@@ -0,0 +1,131 @@
+import 'package:plaid_flutter/plaid_flutter.dart';
+
+import 'package:flutter/material.dart';
+import 'package:moneyup/education/education.dart';
+import 'package:moneyup/main.dart';
+import 'package:moneyup/profile.dart';
+
+class TransactionsHome extends StatelessWidget {
+  const TransactionsHome({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        title: Padding(
+          padding: EdgeInsets.only(top: 10, left: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: const Color.fromARGB(0, 255, 255, 255),
+                  border: Border.all(
+                    width: 3,
+                    color: const Color.fromARGB(255, 121, 121, 121),
+                  ),
+                ),
+                child: Image.asset('assets/icons/profileIcon.png'),
+              ),
+              Container( // NOTIFICATION ICON
+                alignment: Alignment.topRight,
+                padding: EdgeInsets.all(5),
+                child: IconButton(
+                  onPressed: () {
+                    // print('Notification icon pressed');
+                  }, 
+                  icon: Icon(
+                    Icons.notifications_outlined, 
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        toolbarHeight: 120,
+      ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset( // BACKGROUND
+              'assets/images/mu_bg.png',
+              fit: BoxFit.fill
+            ),
+          ),
+          SafeArea( // WHITE BOX CONTAINER
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(50.0),
+                ),
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: const Color.fromARGB(0, 255, 253, 249),
+        height: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Image.asset('assets/icons/unselectedHomeIcon.png'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (_) => MyHomePage(title: 'MoneyUp',),
+                  ),
+                );
+              },
+            ),
+            IconButton(
+              icon: Image.asset('assets/icons/transactionsIcon.png'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (_) => TransactionsHome(),
+                  ),
+                );
+              },
+            ),
+            IconButton(
+              icon: Image.asset('assets/icons/unselectedEducationIcon.png'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (_) => EducationScreen(),
+                  ),
+                );
+              },
+            ),
+            IconButton(
+              icon: Image.asset('assets/icons/unselectedSettingsIcon.png'),
+              onPressed: () {
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (_) => ProfileScreen(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
