a30862a9a1dba31739d541f132f9c7adbb248c2a
Isaiah Adams <iadams1@atu.edu>
2026-02-18 14:06:00 -0600

    Updated the main with the My Wallet features

--- lib/features/proflie/screens/profile.dart
+++ lib/features/proflie/screens/profile.dart
@@ -111,7 +111,7 @@ class ProfileScreen extends StatelessWidget {
                                     padding: const EdgeInsets.all(8.0),
                                     child: Center(
                                       child: Text(
-                                        "Logout",
+                                        "Logout", 
                                         style: TextStyle(
                                           color: Colors.white,
                                           fontSize: 22,
